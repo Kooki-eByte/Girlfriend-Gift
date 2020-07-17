84a4eaaae0a50fbe96bd513a1eb62b8af7541bbd
Cristian Hornedo <officialcristianl@gmail.com>
2020-07-17 15:26:58 -0400

    HTML, put in all the tags needed to show the text, images and a div for a potential gif to be spawned into. CSS, added all the styling and the background image to the page. JS, added a Giphy API to be accessed once the user opens the page and will show the gif once the button is pressed. Also put event listeners to the images for a surprise message

---
+++ script.js
@@ -0,0 +1,54 @@
+// giphy links in a array
+let queryURLArr = [
+  "https://api.giphy.com/v1/gifs/search?api_key=3r8OTeEvptDRXOQTQhqUOjKDdPjJyObL&limit=1&q=i+love+you",
+  "https://api.giphy.com/v1/gifs/search?api_key=3r8OTeEvptDRXOQTQhqUOjKDdPjJyObL&limit=1&q=you+are+the+best",
+  "https://api.giphy.com/v1/gifs/search?api_key=3r8OTeEvptDRXOQTQhqUOjKDdPjJyObL&limit=1&q=heart",
+  "https://api.giphy.com/v1/gifs/search?api_key=3r8OTeEvptDRXOQTQhqUOjKDdPjJyObL&limit=1&q=my+love",
+  "https://api.giphy.com/v1/gifs/search?api_key=3r8OTeEvptDRXOQTQhqUOjKDdPjJyObL&limit=1&q=my+princess",
+];
+
+let gifArr;
+let ranNum = Math.floor(Math.random() * queryURLArr.length);
+// html elements in vars
+let loveBtn = $(".loveBtn");
+let gifDiv = $(".giphyDiv");
+let imgOne = $("#img1");
+let imgTwo = $("#img2");
+let isImgMade = false;
+
+function createElement(gif) {
+  if (!isImgMade) {
+    let newGif = $("<img>");
+    newGif.attr("src", gif);
+    newGif.attr("id", "img-gif");
+    gifDiv.append(newGif);
+    isImgMade = true;
+  } else {
+    $("#img-gif").attr("src", gif);
+  }
+}
+
+$.ajax({
+  url: queryURLArr[ranNum],
+  method: "GET",
+}).then(function (response) {
+  console.log(response.data);
+  //   newGif.attr("src", response.data[0].images.original.url);
+  let theGif = response.data[0].images.original.url;
+  loveBtn.on("click", () => {
+    console.log(theGif);
+    createElement(theGif);
+  });
+});
+
+imgOne.on("click", () => {
+  alert(
+    "The day I went back to see the bestest friend in the world to me, the love of my life. You babess! Thank you for everything you do and so much more my love. Only we could really make things work out the way we do despite any trials"
+  );
+});
+
+imgTwo.on("click", () => {
+  alert(
+    "Being together with you is a dream come true baby, You are the one i need, the one i want, and the one that fits in my heart. So please stay with me forever baby! you are my world and i do all this for us to grow and have a happy life now and forever more! You are my baby girl and i couldnt be any more happier because now i have you next to me either like this or next to me in my heart and soul! I love you my princess!!!"
+  );
+});
